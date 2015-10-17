6a5c94e889d53384f86c39e098e905e08c89dbad
Matthew Stromberg <mcmcmatthew@gmail.com>
2015-10-17 16:00:11 -0500

    add boss health system

---
+++ JollyGame/teamjollygame2-master/Assets/BossHealth.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BossHealth : MonoBehaviour {
+
+    public int startingHealth = 1000;
+    public int currentHealth;
+
+	// Use this for initialization
+	void Start () {
+        currentHealth = startingHealth;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "HeroShot")
+        {
+            takeDamage(10);
+        }
+
+        //play impact sound effect
+
+        //impact particle effect
+
+        Destroy(other.gameObject);
+    }
+
+    void takeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        if (currentHealth <= 0)
+        {
+            die();
+        }
+    }
+
+    void die()
+    {
+        Destroy(gameObject);
+    }
+}
