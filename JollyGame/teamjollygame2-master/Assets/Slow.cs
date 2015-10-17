840accb45d7ec3ffa1e5772ae112bb9acba9ed95
Justin Hill <jhillkwaj@gmail.com>
2015-10-17 17:35:18 -0500

    Added code to slow down small ship when hit.

---
+++ JollyGame/teamjollygame2-master/Assets/Slow.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Slow : MonoBehaviour
+{
+
+    float fullSpeedX = 20f;
+    float fullSpeedY = 20f;
+
+    Movement movementScript;
+
+    void Start()
+    {
+        movementScript = this.GetComponent<Movement>();
+        fullSpeedX = movementScript.maxHSpeed;
+        fullSpeedY = movementScript.maxVSpeed;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+
+        if (other.gameObject.tag == "BossShot")
+        {
+            Destroy(other);
+            movementScript.maxVSpeed = movementScript.maxVSpeed / 2;
+            movementScript.maxHSpeed = movementScript.maxHSpeed / 2;
+        }
+    }
+
+    void Update()
+    {
+        movementScript.maxHSpeed = Mathf.Lerp(movementScript.maxHSpeed, fullSpeedX, .1f * Time.deltaTime);
+        movementScript.maxVSpeed = Mathf.Lerp(movementScript.maxVSpeed, fullSpeedY, .1f * Time.deltaTime);
+    }
+}
