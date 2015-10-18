d32054ee6a1e70151de38a42a81415a611e9266a
Justin Hill <jhillkwaj@gmail.com>
2015-10-18 11:42:17 -0500

    Added stun

---
+++ JollyGame/teamjollygame2-master/Assets/SpawnNPShot.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnNPShot : MonoBehaviour {
+
+    public GameObject target;
+    public float fireTime;
+    float timeToFire;
+
+    void Update()
+    {
+        timeToFire -= Time.deltaTime;
+        if (timeToFire < 0)
+        {
+            timeToFire = fireTime;
+            fire();
+        }
+    }
+
+    void fire()
+    {
+        GameObject shot = Instantiate(Resources.Load("Shot NP"), this.transform.position, this.transform.rotation) as GameObject;
+
+        shot.GetComponent<MoveLaser>().velocity = Vector3.Normalize((target.transform.position + (Random.insideUnitSphere*10)) - this.transform.position) * 5;
+    }
+}
