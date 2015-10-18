d058b1dba8a2d24d1a971f1b49828c339f38a66c
Justin Hill <jhillkwaj@gmail.com>
2015-10-17 22:14:04 -0500

    Added death explosions

--- JollyGame/teamjollygame2-master/Assets/BossHealth.cs
+++ JollyGame/teamjollygame2-master/Assets/BossHealth.cs
@@ -6,6 +6,11 @@ public class BossHealth : MonoBehaviour {
     public int startingHealth = 1000;
     public int currentHealth;
 
+    public GameObject showOnDeath;
+
+    bool inDeath = false;
+    float deathTime = -1;
+
 	// Use this for initialization
 	void Start () {
         currentHealth = startingHealth;
@@ -13,7 +18,9 @@ public class BossHealth : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+        deathTime -= Time.deltaTime;
+        if (inDeath && deathTime < 0)
+            die();
 	}
 
     void OnTriggerEnter2D(Collider2D other)
@@ -35,14 +42,16 @@ public class BossHealth : MonoBehaviour {
     {
         currentHealth -= damage;
 
-        if (currentHealth <= 0)
+        if (currentHealth <= 0 && !inDeath)
         {
-            die();
+            showOnDeath.SetActive(true);
+            inDeath = true;
+            deathTime = 2.0f;
         }
     }
 
     void die()
     {
-        Destroy(gameObject);
+        Destroy(this.gameObject);
     }
 }
